b26d14ef3ae681edd0267f9c129db1475dc2f6b2
AlenaMikalaeynaLisava <63248586+AlenaMikalaeynaLisava@users.noreply.github.com>
2020-08-30 17:08:41 +0300

    RoutAdded

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { Route, Switch} from "react-router-dom";
 import "./App.css";
 import Calculator from "./components/Calculator";
 import Output from "./components/Output";
@@ -11,6 +11,18 @@ function App() {
       <Calculator />
       <Output />
       <Show />
+      <Switch>
+             <Route exact path="/" component = {()=> <div>main</div>}>
+             {/* Component = {()=> <div>A</div>} */}
+               {/* <Home /> */}
+             </Route>
+             <Route >
+               404 not found
+            </Route>
+           <Route path="/dashboard">
+             {/* <Dashboard /> */}
+             </Route>
+         </Switch>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { Provider } from "react-redux";
 import store from "./store";
+import BasicRoter from "./routes"
+
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BasicRoter />
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
